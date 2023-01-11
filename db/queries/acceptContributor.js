0cd0ede4469197db8ecf1f84924647f61c6b20e3
William Li <110069721+william-li0128@users.noreply.github.com>
2023-01-11 12:03:44 -0800

    minor change

--- db/queries/acceptContributor.js
+++ db/queries/acceptContributor.js
@@ -7,16 +7,16 @@ const db = require('../connection');
  * @param {string} story_id The id of the story.
  * @return {Promise<[{}]>} A promise to the contributions.
  */
- const acceptContributor = function(contributionId, story_id) {
+ const acceptContributor = function(contributorId, story_id) {
   return db.query(`UPDATE stories
                    SET tellers = array_append(tellers, (
                     SELECT name
                     FROM users
                     WHERE id = $1
                     ))
-                   WHERE id = $2;`, [contributionId, story_id])
+                   WHERE id = $2;`, [contributorId, story_id])
     .then(
-      console.log(`Story ${story_id} has received tales from ${contributionId}`)
+      console.log(`Story ${story_id} has received tales from ${contributorId}`)
       )
     .catch((err) => {
       console.log(err.message);
